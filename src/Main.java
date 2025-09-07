36f4580610733433dbd3e8487db0759f37c8442a
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-07 22:28:25 +0300

    1'st task done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import javax.sound.midi.Soundbank;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Задача 1
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        // Отступ для визуального разделения задач
+        System.out.println();
+
+        // Задача 2
+
     }
 }
